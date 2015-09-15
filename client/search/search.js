dafba645976d36392649f4ce32d3ab6b58b86ef4
AndreyVT <TupeykoAV@gmail.com>
2015-09-15 17:40:15 +0600

    daily commit

--- client/search/search.js
+++ client/search/search.js
@@ -1,31 +1,35 @@
 Template.search.events({
     "click [data-action='search']": function (e, template) {
         e.preventDefault();
-        var searchText = $('#searchText').val();
-        console.log("searchText::", searchText);
+        searchTextValue = '/' + $('#searchText').val() + '/i';
+        console.log("searchTextValue::", searchTextValue);
         //template.searchResults = Items.find({ "name": /searchText/i }).fetch();
-        template.searchResults.clear();
-        var tmpArray = Items.find({}).fetch(); 
-        console.log("tmpArray::", tmpArray.length);
-        for (var i = 0; i < tmpArray.length; i++) {
-            console.log("tmpArray[i]::", tmpArray[i]);
-            template.searchResults.push(tmpArray[i]);
-        }
+        //template.searchResults.clear();
+        var tmpArray;
+        Meteor.call("findItemsByName", searchTextValue);
+        /*, function (err, list) {
+            tmpArray = list;
+            console.log("tmpArray::", tmpArray.length);
+            for (var i = 0; i < tmpArray.length; i++) {
+                console.log("tmpArray[i]::", tmpArray[i]);
+                template.searchResults.push(tmpArray[i]);
+            }
+
+            console.log("template.searchResults::", template.searchResults);
+            $('#searchText').val('')
+        });*///Items.find({ "name": searchTextValue }).fetch(); //Items.find({}).fetch();
         
-        console.log("template.searchResults::", template.searchResults);
-        $('#searchText').val('')
     }
 });
 
 Template.search.helpers({
     searchItems: function () {
-        console.log("Template.instance().searchResults.list()::", Template.instance().searchResults.list());
-        return Template.instance().searchResults.list();
+        //return Template.instance().searchResults.list();
+        return searchResults;
     }
 });
 
 Template.search.onCreated(function () {
     this.searchText = new ReactiveVar(0);
-    this.searchResults = new ReactiveArray([]);
-    this.searchResults.thisIs = 1;
+    searchResults = new ReactiveArray([]);
 });
