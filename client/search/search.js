5e6e19493eb504c40eed8fe159317b7fa8abdfca
AndreyVT <TupeykoAV@gmail.com>
2015-09-16 16:23:50 +0600

    Daily work commit

--- client/search/search.js
+++ client/search/search.js
@@ -1,35 +1,22 @@
 Template.search.events({
     "click [data-action='search']": function (e, template) {
         e.preventDefault();
-        searchTextValue = '/' + $('#searchText').val() + '/i';
-        console.log("searchTextValue::", searchTextValue);
-        //template.searchResults = Items.find({ "name": /searchText/i }).fetch();
-        //template.searchResults.clear();
-        var tmpArray;
-        Meteor.call("findItemsByName", searchTextValue);
-        /*, function (err, list) {
-            tmpArray = list;
-            console.log("tmpArray::", tmpArray.length);
-            for (var i = 0; i < tmpArray.length; i++) {
-                console.log("tmpArray[i]::", tmpArray[i]);
-                template.searchResults.push(tmpArray[i]);
-            }
-
-            console.log("template.searchResults::", template.searchResults);
-            $('#searchText').val('')
-        });*///Items.find({ "name": searchTextValue }).fetch(); //Items.find({}).fetch();
-        
+        template.searchResults.clear();
+        var tmpArray = Items.find({ "name": new RegExp($('#searchText').val(), 'i') }).fetch();
+        template.searchResults.clear();
+        for (var i = 0; i < tmpArray.length; i++) {
+            template.searchResults.push(tmpArray[i]);
+        }
     }
 });
 
 Template.search.helpers({
     searchItems: function () {
-        //return Template.instance().searchResults.list();
-        return searchResults;
+        return Template.instance().searchResults.list();
     }
 });
 
 Template.search.onCreated(function () {
     this.searchText = new ReactiveVar(0);
-    searchResults = new ReactiveArray([]);
+    this.searchResults = new ReactiveArray([]);
 });
--- client/search/search_item.js
+++ client/search/search_item.js
@@ -1,1 +1,12 @@
-﻿
+﻿Template.searchItem.helpers({
+    minPrice: function () {
+        return Session.get("miPrice" + this._id) || "Loading...";
+    },
+    maxPrice: function () {
+        return this._id;
+    }
+});
+
+Template.searchItem.onCreated(function () {
+   // Meteor.call("findItemMinPrice", this._id);
+});
